fbc3300a4266cb5202cc54f27d50e57ec7ca13b2
Mikov Denis <45800215+TaggedDev@users.noreply.github.com>
2026-04-10 22:07:55 +0300

    Added NumSharp .npy file read

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using NumSharp;
+
+namespace MotionDataVisualization;
+
+internal class Program
+{
+    private static void Main()
+    {
+        const string rootWithBins = @"D:\Code\CSharp\text-to-motion\dataset\new_joint_vecs";
+        string[] files = Directory.GetFiles(rootWithBins, "*.npy")
+            .Where(file => !file.EndsWith(".clip.bin"))
+            .ToArray();
+
+        foreach (var file in files)
+        {
+            Console.WriteLine($"File: {file}");
+            NDArray ndArray = np.load(file);
+            Console.WriteLine(string.Join(", ", ndArray.shape));
+            break;
+        }
+    }
+}
